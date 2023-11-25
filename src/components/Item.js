a43849e9714f836ec7ba0f5ce88801b06c1b4551
Mohammed <khamismitch@gmail.com>
2023-11-25 18:29:25 +0300

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
 import React from "react";
-
+import { useState } from "react";
 function Item({ name, category }) {
+const [list, setList] = useState('')
+  const handleClick = () =>{
+    setList('in-cart')
+  }
   return (
-    <li className="">
+    <li className={list}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>Add to Cart</button>
     </li>
   );
 }
